a3db04acc9df1ec5db924b65137cf41ea2bf1c53
ari.waingold@outlook.com <ari.waingold@outlook.com>
2026-03-05 10:30:02 -0800

    small changes to setNutrient

--- setNutrient.js
+++ setNutrient.js
@@ -1,4 +1,5 @@
 console.log('setNutrient content script loaded!');
+// Nutrient map for names that differ between UW website and Cronometer
 const NUTRIENT_MAP = {
     'Vit. D': 'Vitamin D',
     'Potas.': 'Potassium',
@@ -12,7 +13,6 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         try {
             Object.keys(message.data).forEach((nutrient) => {
                 const cronName = NUTRIENT_MAP[nutrient] || nutrient;
-                console.log('Setting nutrient:', cronName, message.data[nutrient]);
                 setNutrient(cronName, message.data[nutrient]);
             });
             sendResponse({status: "success"});
@@ -26,21 +26,17 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
  * Sets the given nutrient field to the given value
  * @param field the name of the nutrient field
  * @param value the value to set
- * @throws Error if there is no field with name parameter field
  */
 function setNutrient(field, value) {
     const labelDiv = Array.from(document.querySelectorAll('div.GHB44OMBOJ, div.GHB44OMBEK, div.GHB44OMBFK, div.GHB44OMBFJ, tr.GHB44OMBAK div.gwt-Label'))
         .find(div => div.textContent.trim() === field);
-    console.log('labelDiv for', field, ':', labelDiv);
     if (!labelDiv) {
         console.log('Skipping unrecognized nutrient:', field);
         return;
     }
     const row = labelDiv.closest('tr');
     const input = row.querySelector('.GHB44OMBGJ');
-    console.log('input for', field, ':', input);
     const displayDiv = input.previousElementSibling;
-    console.log('displayDiv for', field, ':', displayDiv);
     displayDiv.click();
     input.value = value;
     input.dispatchEvent(new Event('input', { bubbles: true }));
